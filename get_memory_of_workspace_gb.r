7073b1ef18279482611910fd7defff772f38c87e
mo0zi <cdanek@awi.de>
2018-12-12 14:02:50 +0100

    more

--- get_memory_of_workspace_gb.r
+++ get_memory_of_workspace_gb.r
@@ -1,7 +1,13 @@
-get_memory_of_workspace_gb <- function(obj_list) {
+get_memory_of_workspace_gb <- function(obj_list, unit="Mb") {
 
     ws <- sapply(obj_list, function(x) object.size(get(x)))
-    ws <- sort(ws, decreasing=T)/1024^2 # Mb
+    ws <- sort(ws, decreasing=T)
+
+    if (unit == "Mb") {
+        ws <- ws/1024^2
+    } else {
+        stop(paste0("Unit '", unit, "' not defined."))
+    }
 
     return(ws)
 
