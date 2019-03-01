ab735b4b6243dfd566de2bfec3ca80a54b4f7d3c
mo0zi <cdanek@awi.de>
2019-03-01 22:37:13 +0100

    more

---
+++ mylocator.r
@@ -0,0 +1,7 @@
+mylocator <- function(...) {
+
+    options(locatorBell=F) # turn off system beep
+    print("To exit the locator, hit any mouse button but the first while the mouse is over the plot device ...")
+    locator(type="o", ...)
+
+} # mylocator
