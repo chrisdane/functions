9d4f205ff38482295461da8f50d27c8fdc073376
mo0zi <cdanek@awi.de>
2019-03-03 15:57:27 +0100

    more

--- load_package.r
+++ load_package.r
@@ -2,28 +2,28 @@ load_package <- function(packagename) {
 
     if (!any(search() == paste0("package:", packagename))) {
         if (verbose > 0) {
-            print(paste0(indent, "Load '", packagename, "' package ..."))
+            message(paste0(indent, "Load '", packagename, "' package ..."))
         }
         suppressMessages(suppressWarnings(require(packagename, character.only=T)))
         if (!any(search() == paste0("package:", packagename))) {
-            print(paste0(indent, "Warning: Could not load '", packagename, "' package from"))
-            print(paste0(indent, "         .libPaths=", ifelse(length(.libPaths()) > 1, "c('", "'"),
+            message(paste0(indent, "Warning: Could not load '", packagename, "' package from"))
+            message(paste0(indent, "         .libPaths=", ifelse(length(.libPaths()) > 1, "c('", "'"),
                          paste0(.libPaths(), collapse="','"), ifelse(length(.libPaths()) > 1, "')", "'"), "."))
-            print(paste0(indent, "         You can add library paths by providing"))
-            print(paste0(indent, "            rpackagepaths='/path/to/installed/libraries/' or"))
-            print(paste0(indent, "            rpackagepaths=c('/path1/to/packages/', 'path2/to/packages/')"))
-            print(paste0(indent, "         in namelist.rfesom.r or you can install the package with"))
-            print(paste0(indent, "            install.packages('", packagename, "')."))
+            message(paste0(indent, "         You can add library paths by providing"))
+            message(paste0(indent, "            rpackagepaths='/path/to/installed/libraries/' or"))
+            message(paste0(indent, "            rpackagepaths=c('/path1/to/packages/', 'path2/to/packages/')"))
+            message(paste0(indent, "         in namelist.rfesom.r or you can install the package with"))
+            message(paste0(indent, "            install.packages('", packagename, "')."))
             success <- F
         } else {
             if (verbose > 0) {
-                #print(paste0(indent, "Done."))
+                #message(paste0(indent, "Done."))
             }
             success <- T
         }
     } else {
         if (verbose > 0) {
-            #print(paste0(indent, "Package '", packagename, "' is already loaded ..."))
+            #message(paste0(indent, "Package '", packagename, "' is already loaded ..."))
         }
         success <- T
     }
