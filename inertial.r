6bbbe507861d3b3ab9c7c3b50ce690645c508a45
Christopher Danek <cdanek@posteo.de>
2019-03-06 19:00:56 +0100

    more

---
+++ inertial.r
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+phi <- c(0.1, 1, 2, 3, 4, 5, 7.5, seq(10, 90, b=5))
+omega <- 2*pi/86400
+f <- 2*omega*sin(phi*pi/180)
+inertial_period_sec <- 1/f
+inertial_period_min <- inertial_period_sec/60
+inertial_period_hour <- inertial_period_sec/(60*60)
+inertial_period_day <- inertial_period_sec/(24*60*60)
+inertial_period_month <- inertial_period_sec/(30*24*60*60)
+inertial_period_year <- inertial_period_sec/(12*30*24*60*60)
+
+df <- data.frame(phi=phi, f=f,
+                 sec=inertial_period_sec,
+                 min=inertial_period_min,
+                 hour=inertial_period_hour,
+                 day=inertial_period_day,
+                 month=inertial_period_month,
+                 year=inertial_period_year
+                 )
+print(df, digits=3)
