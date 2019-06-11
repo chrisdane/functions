24bc6d2bfca7559f3030be3e4c3ce4e8cd213eb8
Christopher Danek <cdanek@posteo.de>
2019-06-11 17:07:19 +0200

    more

--- rfesom_varlist2readme.r
+++ rfesom_varlist2readme.r
@@ -6,6 +6,11 @@ args <- commandArgs(trailingOnly=T)
 namelist.var_fname <- args[1]
 readme <- args[2]
 
+if (F) { # testing
+    namelist.var.fname <- "/home/mozi/scripts/r/rfesom/namelists/namelist.var.r"
+    readme <- "test"
+}
+
     message("update ", readme, " based on ", namelist.var_fname, " ...")
 
     # read rfesom variable namelist entries
@@ -19,9 +24,11 @@ readme <- args[2]
                        stop=nchar(namelist))
     # replace ""
     namelist <- gsub("\"", "", namelist)
-    
+    # replace |
+    namelist <- gsub("\\|", "<code>\\\\|</code>", namelist)
+
     # append varname list to rfesom readme
-    system(paste0("echo '## Available variables' >> ", readme))
+    system(paste0("echo '## Available variables  ' >> ", readme))
     system(paste0("echo >> ", readme))
     system(paste0("echo '(automated list based on ", basename(namelist.var_fname), ")' >> ", readme))
     system(paste0("echo >> ", readme))
