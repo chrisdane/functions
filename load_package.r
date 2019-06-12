0a205cae794e3335f148794791c3bc018930bd07
Christopher Danek <cdanek@posteo.de>
2019-06-12 12:30:09 +0200

    More

--- load_package.r
+++ load_package.r
@@ -6,14 +6,14 @@ load_package <- function(packagename) {
         }
         suppressMessages(suppressWarnings(require(packagename, character.only=T)))
         if (!any(search() == paste0("package:", packagename))) {
-            message(paste0(indent, "Warning: Could not load '", packagename, "' package from"))
-            message(paste0(indent, "         .libPaths=", ifelse(length(.libPaths()) > 1, "c('", "'"),
+            message(paste0(indent, "Could not load '", packagename, "' package from"))
+            message(paste0(indent, "   .libPaths=", ifelse(length(.libPaths()) > 1, "c('", "'"),
                          paste0(.libPaths(), collapse="','"), ifelse(length(.libPaths()) > 1, "')", "'"), "."))
-            message(paste0(indent, "         You can add library paths by providing"))
-            message(paste0(indent, "            rpackagepaths='/path/to/installed/libraries/' or"))
-            message(paste0(indent, "            rpackagepaths=c('/path1/to/packages/', 'path2/to/packages/')"))
-            message(paste0(indent, "         in namelist.rfesom.r or you can install the package with"))
-            message(paste0(indent, "            install.packages('", packagename, "')."))
+            message(paste0(indent, "You can add library paths by providing"))
+            message(paste0(indent, "   rpackagepaths='/path/to/installed/libraries/' or"))
+            message(paste0(indent, "   rpackagepaths=c('/path1/to/packages/', 'path2/to/packages/')"))
+            message(paste0(indent, "in namelist.rfesom.r or you can install the package with"))
+            message(paste0(indent, "   install.packages('", packagename, "')."))
             success <- F
         } else {
             if (verbose > 0) {
