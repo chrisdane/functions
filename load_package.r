e8544d0daff42907b416a3e6ad33b6ef8d02ea79
Christopher Danek <cdanek@posteo.de>
2019-06-12 12:33:30 +0200

    more

--- load_package.r
+++ load_package.r
@@ -6,6 +6,7 @@ load_package <- function(packagename) {
         }
         suppressMessages(suppressWarnings(require(packagename, character.only=T)))
         if (!any(search() == paste0("package:", packagename))) {
+            message(paste0(indent, "****** load_package() ******"))
             message(paste0(indent, "Could not load '", packagename, "' package from"))
             message(paste0(indent, "   .libPaths=", ifelse(length(.libPaths()) > 1, "c('", "'"),
                          paste0(.libPaths(), collapse="','"), ifelse(length(.libPaths()) > 1, "')", "'"), "."))
@@ -13,7 +14,8 @@ load_package <- function(packagename) {
             message(paste0(indent, "   rpackagepaths='/path/to/installed/libraries/' or"))
             message(paste0(indent, "   rpackagepaths=c('/path1/to/packages/', 'path2/to/packages/')"))
             message(paste0(indent, "in namelist.rfesom.r or you can install the package with"))
-            message(paste0(indent, "   install.packages('", packagename, "')."))
+            message(paste0(indent, "   install.packages('", packagename, "')"))
+            message(paste0(indent, "****************************"))
             success <- F
         } else {
             if (verbose > 0) {
