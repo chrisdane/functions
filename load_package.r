0a71fcc554e6f34d24f71acb5e7b0d80dc7e2efe
Christopher Danek <cdanek@posteo.de>
2019-06-13 13:31:59 +0200

    more

--- load_package.r
+++ load_package.r
@@ -1,10 +1,15 @@
 load_package <- function(packagename) {
+        
+    if (!exists("verbose")) verbose <- 1
 
+    # need to load package
     if (!any(search() == paste0("package:", packagename))) {
         if (verbose > 0) {
             message(paste0(indent, "Load '", packagename, "' package ..."))
         }
         suppressMessages(suppressWarnings(require(packagename, character.only=T)))
+        
+        # no success
         if (!any(search() == paste0("package:", packagename))) {
             message(paste0(indent, "****** load_package() ******"))
             message(paste0(indent, "Could not load '", packagename, "' package from"))
@@ -17,12 +22,16 @@ load_package <- function(packagename) {
             message(paste0(indent, "   install.packages('", packagename, "')"))
             message(paste0(indent, "****************************"))
             success <- F
+
+        # success
         } else {
             if (verbose > 0) {
                 #message(paste0(indent, "Done."))
             }
             success <- T
         }
+    
+    # package is already loaded
     } else {
         if (verbose > 0) {
             #message(paste0(indent, "Package '", packagename, "' is already loaded ..."))
