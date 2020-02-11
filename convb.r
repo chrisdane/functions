ceb103aea13a3f057206f9c37646fdbb75773bdf
Christopher Danek <cdanek@posteo.de>
2020-02-11 16:02:41 +0100

    asd

--- convb.r
+++ convb.r
@@ -4,7 +4,7 @@ convb <- function(x, unit) {
     # convert human readable to bytes
     # https://stackoverflow.com/questions/10910688/converting-kilobytes-megabytes-etc-to-bytes-in-r/49380514
 
-    if (!is.finite(x) || !is.character(unit)) stop("x must be finite and unit must be character")
+    if (any(!is.finite(x)) || any(!is.character(unit))) stop("x must be finite and unit must be character")
     unit[unit == ""] <- "1"
     mult <- c("1"=1,  "B"=1,  "K"=1024,  "M"=1024^2,  "G"=1024^3,  "T"=1024^4,  "P"=1024^5)
     x * unname(mult[unit])
