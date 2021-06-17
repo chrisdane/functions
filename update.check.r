c01b3430aebc28b6c35dbb1baac58d1e64e6ef4e
chrisdane <cdanek@awi.de>
2021-06-17 10:53:33 +0200

    asd

--- update.check.r
+++ update.check.r
@@ -1,27 +1,35 @@
 # r
 
 update.check <- function() {
-    options(width=3000) # increase length per print line from default 80
-    inst <- packageStatus()
-    inds <- which(inst$inst$Status == "upgrade") # ok upgrade unavailable
-    if (length(inds) > 0) {
-        message("These packages need an update:")
-        print(inst$inst[inds,c("Package", "Version", "Status", "LibPath")])
-        message("")
-        yn <- menu(choices=c("yes", "no"), 
-                   title="Run utils::update.packages(instlib=.libPaths()[1], ask=F, checkBuilt=T) now?")
-        if (yn == 1) {
-            utils::update.packages(instlib=.libPaths()[1], ask=F, checkBuilt=T)
+    libpaths <- .libPaths()
+    yn <- rep(F, t=length(libpaths)) # default: dont update
+    # first loop just show packages to upgrade
+    for (libpathi in seq_along(libpaths)) {
+        message("*************************************************\n",
+                "libpath ", libpathi, "/", length(libpaths), ": ", libpaths[libpathi])
+        if (file.access(libpaths[libpathi], mode=2) == -1) { # dir not writeable
+            message("--> directory not writeable. skip")
+        } else {
+            inst <- packageStatus(lib=libpaths[libpathi])
+            inds <- which(inst$inst$Status == "upgrade") # ok upgrade unavailable
+            if (length(inds) > 0) {
+                message("These ", length(inds), " packages need an update:")
+                print(inst$inst[inds,c("Package", "Version", "Status")])
+            }
+            yn[libpathi] <- askYesNo(paste0("\nrun `utils::update.packages(lib=\"", libpaths[libpathi], 
+                                            "\", instlib=\"", libpaths[libpathi], "\", ask=F, checkBuilt=T)`?"))
         }
-    } else {
-        message("All packages uptodate.")
-    }
-    inds <- which(inst$inst$Status == "unavailable")
-    if (length(inds) > 0) {
-        message("However, these packages are unavailable:")
-        print(inst$inst[inds,c("Package", "Version", "Status", "LibPath")])
-    }
-
+    } # for libpathi
+    if (any(is.na(yn))) stop("this should not happen")
+    # second loop: actual update
+    for (libpathi in seq_along(yn)) {
+        if (yn[libpathi]) {
+            message("\n***************************************************\n",
+                    "run `utils::update.packages(lib=\"", libpaths[libpathi], "\", instlib=\"", 
+                    libpaths[libpathi], "\", ask=F, checkBuilt=T)` ...\n")
+            utils::update.packages(lib=libpaths[libpathi], instlib=libpaths[libpathi], ask=F, checkBuilt=T)
+        }
+    } # if any packages need update and update is wanted
     #return(inst)
 } # update.check
 
